24873f217d50f058deb3e2b5387acca7c51d5bec
Eli Cusic <tntitans31@gmail.com>
2020-09-17 16:27:51 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
+# require 'sqlite3'
+# DB = {:conn => SQLite3::Database.new("db/pokemon.db")}
+require 'pry'
 class Pokemon
+    attr_accessor :name, :type
+    attr_reader :id, :db
+
+    def initialize(hash)
+        @id = hash[:id]
+        @name = hash[:name]
+        @type = hash[:type]
+        @db = hash[:db]
+    end
+
+    def self.save(name, type, db)
+        sql = "INSERT INTO pokemon (name,type) VALUES (?,?)"
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        array = db.execute(sql,id)[0]
+        new_poke = Pokemon.new({id: array[0], name: array[1], type: array[2]})
+    end
 end
